24a72482388eea07fac59a3943712e5f0d2a2e97
Uchio Kondo <u-kondo@fujisan.co.jp>
2011-07-04 12:45:24 +0900

    ready to deploy

--- Gemfile
+++ Gemfile
@@ -1,5 +1,10 @@
 source :rubygems
+gem 'bundler'
+
 gem 'rack'
 gem 'sinatra'
 gem 'haml'
 gem 'sass'
+
+gem 'capistrano', :require => false
+
---
+++ config/deploy.rb
@@ -0,0 +1,25 @@
+require 'bundler/capistrano'
+
+set :application, "bingo"
+set :repository,  "git://github.com/udzura/jqbingo.git"
+
+set :scm, :git
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+role :web, "udzura.jp"                          # Your HTTP server, Apache/etc
+role :app, "udzura.jp"                          # This may be the same as your `Web` server
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+set :deploy_to, "/var/www/bingo"
+
+
+# If you are using Passenger mod_rails uncomment this:
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
